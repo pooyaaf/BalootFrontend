c94b0b2e1910f5af804a0a615a328578212240fa
pooyaaf <pooya1380@gmail.com>
2023-04-19 17:49:46 +0430

    add modal user page

--- src/components/Page/User/User.jsx
+++ src/components/Page/User/User.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
 import product from "../../../assets/img/commodity.png";
 import productmamaspagetti from "../../../assets/img/momsSp.png";
 import productmic from "../../../assets/img/mic.png";
@@ -14,6 +14,10 @@ import "../../Common/style.css";
 import "./User.css";
 
 const User = () => {
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+
   return (
     <main>
       <div className="user-box">
@@ -108,10 +112,74 @@ const User = () => {
               </tbody>
             </table>
           </div>
-          <button type="submit" className="pay-now-button btn">
+          <button
+            type="button"
+            className="pay-now-button btn"
+            onClick={handleShow}
+          >
             Pay now!
           </button>
         </div>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Your Cart</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {
+              /* Items List */
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <img
+                        src={product}
+                        alt="Product Image"
+                        className="table-image"
+                      />
+                    </td>
+                    <td>Galaxy S21</td>
+                    <td>$21000000</td>
+                  </tr>
+                </tbody>
+              </table>
+            }
+            {
+              /* Discount Code Input */
+              <div className="form-group">
+                <label htmlFor="discountCode">Discount Code:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="discountCode"
+                  placeholder="Enter discount code"
+                />
+              </div>
+            }
+            {
+              /* Total Price */
+              <div className="total-price">
+                <h5>Total Price: $21000000</h5>
+              </div>
+            }
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="btn-link  modalclose" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary " onClick={handleClose}>
+              Buy
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
         <div className="history">
           <p className="section-title">
             <img src={historyicon} alt="History" className="History" />
