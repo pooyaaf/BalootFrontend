5795d094374b933703075b901155ef999f9312ff
alireza <alirezaesmaeilzadeh113@gmail.com>
2023-05-05 01:09:28 +0330

    Fix register.

--- src/components/Page/Auth/Register.jsx
+++ src/components/Page/Auth/Register.jsx
@@ -1,11 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../../assets/img/logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../../Common/style.css";
 import "./Login.css";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthdate, setBirthdate] = useState("");
+  const [address, setAddress] = useState("");
+
+  function handleUsernameChange(event) {
+    setUsername(event.target.value);
+  }
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+  function handleBirthdateChange(event) {
+    setBirthdate(event.target.value);
+  }
+  function handleAddressChange(event) {
+    setAddress(event.target.value);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const response = await fetch(
+      `http://127.0.0.1:8080/register?username=${username}&password=${password}&email=${email}&birthdate=${birthdate}&address=${address}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        method: "POST",
+        mode: "cors",
+        redirect: "follow",
+      }
+    ).then((response) => {
+      if (response.ok) {
+        alert("Register Successful!");
+        localStorage.setItem("username", username);
+        navigate("/");
+      } else {
+        alert("Wrong username or password!");
+        navigate("/register");
+      }
+      return response.json();
+    });
+    console.log("A name was submitted: " + response.body);
+  }
   return (
     <div className="container mt-5">
       <div className="row">
@@ -18,13 +68,15 @@ const Register = () => {
               <h4>Sign Up</h4>
             </div>
             <div className="card-body">
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div className="form-group">
                   <label for="username">Username</label>
                   <input
                     type="text"
                     className="form-control"
                     id="username"
+                    value={username}
+                    onChange={handleUsernameChange}
                     placeholder="Enter username"
                   />
                 </div>
@@ -34,6 +86,8 @@ const Register = () => {
                     type="password"
                     className="form-control"
                     id="password"
+                    value={password}
+                    onChange={handlePasswordChange}
                     placeholder="Enter password"
                   />
                 </div>
@@ -43,6 +97,8 @@ const Register = () => {
                     type="email"
                     className="form-control"
                     id="email"
+                    value={email}
+                    onChange={handleEmailChange}
                     placeholder="Enter email"
                   />
                 </div>
@@ -52,6 +108,8 @@ const Register = () => {
                     type="date"
                     className="form-control"
                     id="birthdate"
+                    value={birthdate}
+                    onChange={handleBirthdateChange}
                     placeholder="Enter birthdate"
                   />
                 </div>
@@ -60,6 +118,8 @@ const Register = () => {
                   <textarea
                     className="form-control"
                     id="address"
+                    value={address}
+                    onChange={handleAddressChange}
                     rows="3"
                     placeholder="Enter address"
                   ></textarea>
