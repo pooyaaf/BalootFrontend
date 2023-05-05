936d73d235fbe0fc370c13aefa20cd06392827d8
alireza <alirezaesmaeilzadeh113@gmail.com>
2023-05-05 07:45:37 +0330

    Fix provider.

--- src/components/Page/Provider/Provider.jsx
+++ src/components/Page/Provider/Provider.jsx
@@ -6,31 +6,44 @@ import "./Provider.css";
 import CommodityCard from "../../Common/CommodityCard";
 import { useParams } from "react-router-dom";
 
-
 const Provider = () => {
   const { id } = useParams();
   const [commodities, setCommodities] = useState([]);
   const [providerInfo, setProviderInfo] = useState({});
+  const [quantities, setQuantities] = useState({});
+
+  const handleIncrement = (id) => {
+    setQuantities({ ...quantities, [id]: (quantities[id] || 0) + 1 });
+  };
+
+  const handleDecrement = (id) => {
+    if (quantities[id] > 0) {
+      setQuantities({ ...quantities, [id]: quantities[id] - 1 });
+    }
+  };
+
+  const getQuantity = (id) => {
+    return quantities[id] || 0;
+  };
 
   useEffect(() => {
-    fetch(
-      `http://127.0.0.1:8080/providers/${id}`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        method: "GET",
-        mode: "cors",
-      }
-    ).then((response) => {
-      if (response.ok) {
-        console.log("Commodity fetch succussfuly");
-      } else {
-        console.log("Commodity fetch unsuccussfuly!");
-      }
-      return response.json();
-    }).then(data => setProviderInfo(data));
+    fetch(`http://127.0.0.1:8080/providers/${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      method: "GET",
+      mode: "cors",
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("Commodity fetch succussfuly");
+        } else {
+          console.log("Commodity fetch unsuccussfuly!");
+        }
+        return response.json();
+      })
+      .then((data) => setProviderInfo(data));
   }, [id]);
 
   useEffect(() => {
@@ -40,20 +53,36 @@ const Provider = () => {
     );
     setCommodities(filteredCommodities);
   }, []);
-  if (providerInfo.providerModel === undefined)
-    return(<></>);
+  if (
+    providerInfo.providerModel === undefined ||
+    providerInfo.commoditiesList === undefined
+  )
+    return <></>;
   return (
     <div>
       <div className="centered">
-        <img src={providerInfo.providerModel.image} className="provider-logo" alt="Provider" />
-        <h4 className="date-provider">since {providerInfo.providerModel.registryDate}</h4>
+        <img
+          src={providerInfo.providerModel.image}
+          className="provider-logo"
+          alt="Provider"
+        />
+        <h4 className="date-provider">
+          since {providerInfo.providerModel.registryDate}
+        </h4>
         <h2 className="provider-title">{providerInfo.providerModel.name}</h2>
       </div>
       <div>
         <h2 className="all-provided-commodities">All provided commodities</h2>
-        {commodities.map((commodity) => (
-          <CommodityCard key={commodity.id} commodity={commodity} />
-        ))}
+        <div className="commodities-list">
+          {providerInfo.commoditiesList.map((commodity) => (
+            <CommodityCard
+              commodity={commodity}
+              handleIncrement={handleIncrement}
+              handleDecrement={handleDecrement}
+              getQuantity={getQuantity}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
