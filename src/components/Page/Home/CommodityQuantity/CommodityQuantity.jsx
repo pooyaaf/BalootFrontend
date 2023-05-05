599b160279a86912f12dfc5534c187398aea29a0
alireza <alirezaesmaeilzadeh113@gmail.com>
2023-05-05 20:37:02 +0330

    Complete Add and Remove buylist.

--- src/components/Page/Home/CommodityQuantity/CommodityQuantity.jsx
+++ src/components/Page/Home/CommodityQuantity/CommodityQuantity.jsx
@@ -7,14 +7,60 @@ const CommodityQuantity = ({
   handleDecrement,
   getQuantity,
 }) => {
+  const handleAddToBuyListSubmit = () => {
+    handleAddToBuyList();
+    handleIncrement(commodity.id);
+  };
+  const handleRemoveFromBuyListSubmit = () => {
+    handleRemoveFromBuyList();
+    handleDecrement(commodity.id);
+  };
+  async function handleAddToBuyList() {
+    const response = await fetch(
+      `http://127.0.0.1:8080/addToBuyList/${commodity.id}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        method: "POST",
+        mode: "cors",
+        redirect: "follow",
+      }
+    );
+    if (response.ok) {
+      console.log("Add to buyLit succussfuly");
+    } else {
+      console.log("Add to buyLit unsuccussfuly!");
+    }
+  }
+  async function handleRemoveFromBuyList() {
+    const response = await fetch(
+      `http://127.0.0.1:8080/removeFromBuyList?commodityId=${commodity.id}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        method: "POST",
+        mode: "cors",
+        redirect: "follow",
+      }
+    );
+    if (response.ok) {
+      console.log("Remove from buyLit succussfuly");
+    } else {
+      console.log("Remove from buyLit unsuccussfuly!");
+    }
+  }
   return (
     <td>
       <div className="d-flex align-items-center justify-content-center">
         <div className="in-card">
           <span>
-            <Button onClick={() => handleDecrement(commodity.id)}>-</Button>
+            <Button onClick={handleRemoveFromBuyListSubmit}>-</Button>
             {getQuantity(commodity.id)}
-            <Button onClick={() => handleIncrement(commodity.id)}>+</Button>
+            <Button onClick={handleAddToBuyListSubmit}>+</Button>
           </span>
         </div>
       </div>
